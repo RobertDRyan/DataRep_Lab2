7ad2d609886527aeb7bf6e419764aca9108da8a0
RobertDRyan <43645804+RobertDRyan@users.noreply.github.com>
2019-09-29 12:18:46 +0100

    Add files via upload
    Finished

---
+++ BankingApp/src/app/mortgage/mortgage.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { InterestService } from "../Services/interest.service";
+
+
+@Component({
+  selector: 'app-mortgage',
+  templateUrl: './mortgage.component.html',
+  styleUrls: ['./mortgage.component.css']
+})
+export class MortgageComponent implements OnInit {
+
+ 
+
+  constructor(private interestService:InterestService) { }
+
+  Total: number;
+  years: number;
+
+calaulateinterest(): void {
+  this.Total = this.interestService.calaulateinterest(this.years);
+  } 
+
+  ngOnInit() {
+  }
+
+}
